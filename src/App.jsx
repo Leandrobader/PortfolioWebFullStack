da07cda0cbfcc90d8723998c247844dd218d7cd4
Leandro Bader <baderleandro@gmail.com>
2024-06-24 11:30:42 -0300

    Se instalo ReactRouterDom y se creo la carpeta Components con el Home, Navbar y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './Components/Nav';
+import Foot from './Components/Foot';
+import Home from './Components/Pages/Home';
 
 function App() {
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+    <BrowserRouter>
+    <header>
+    <Nav></Nav>
+    </header>
+
+    <main>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+    </Routes>
+    </main>
+
+    <footer>
+    <Foot></Foot>
+    </footer>
+    </BrowserRouter>
     </>
   )
 }
